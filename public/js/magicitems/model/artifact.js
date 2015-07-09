9b4e43c4935a13a2d376d626689b82b0fd0812ad
David Kanenwisher <d.kanen@gmail.com>
2015-07-09 11:17:53 -0500

    A little extra comment.

--- public/js/magicitems/model/artifact.js
+++ public/js/magicitems/model/artifact.js
@@ -6,7 +6,7 @@ var MagicItems = MagicItems || {};
  */
 (function () {
     /**
-     * Creates a new artifact
+     * Creates a new artifact so that you can keep track of them!
      * @param name The name of the artifact.
      * @param description An apt description of the artifacts origins.
      * @param strength The strength bonus the artifact imbues upon it's owner.
