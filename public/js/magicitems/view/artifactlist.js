e2948cc3b9c9e7ef52860c1605413cd9c0a9b7f9
David Kanenwisher <d.kanen@gmail.com>
2015-07-09 21:44:47 -0500

    I created the artifact list view.

---
+++ public/js/magicitems/view/artifactlist.js
@@ -0,0 +1,33 @@
+var MagicItems = MagicItems || {};
+MagicItems.View = MagicItems.View || {};
+
+/**
+ * Represents a collection of magical artifacts.
+ */
+(function () {
+    /**
+     * Draws an Artifact as a tile.
+     * @constructor
+     */
+    MagicItems.View.ArtifactList = function(el, collection) {
+        var parentTag = "ol";
+        var childTag = "li";
+
+        this.render = function() {
+            var domFragment = document.createDocumentFragment();
+            var listElement = document.createElement(parentTag);
+
+            _.forEach(collection.items(), function(item) {
+                var listItemElement = document.createElement(childTag);
+                var view = new MagicItems.View.ArtifactTile(listItemElement, item);
+                view.render();
+
+                listElement.appendChild(listItemElement);
+            });
+
+            domFragment.appendChild(listElement);
+            el.appendChild(domFragment);
+            return this;
+        }
+    };
+})();
